37d65adedb4bd69a1fd01778b12547dbb125d946
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-11-13 11:27:06 +0500

    Updated admin.py

--- src/timetable/admin.py
+++ src/timetable/admin.py
@@ -1,25 +1,57 @@
 from django.contrib import admin
 from .models import *
 
-@admin.register(Faculty, Course, Teacher, Subject, Day, Week, Room)
-class BaseAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
 
-class ScheduleInline(admin.TabularInline):
-    model = Schedule
-    extra = 1
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('number', 'faculty')
+    list_filter = ('faculty',)
+
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
-    inlines = [ScheduleInline]
+    list_display = ('name', 'course')
+    list_filter = ('course__faculty', 'course__number')
 
-@admin.register(Schedule)
-class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ('group', 'day', 'week', 'lesson_number', 'subject', 'teacher', 'room', 'lesson_type')
-    list_filter = ('group', 'day', 'week', 'room')
-    search_fields = ('subject__name', 'teacher__full_name', 'room__name')
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'email')
+
+
+@admin.register(Week)
+class WeekAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('order', 'name')
+    ordering = ('order',)
+
 
 @admin.register(LessonNumber)
 class LessonNumberAdmin(admin.ModelAdmin):
-    list_display = ('pair_number', 'start_time', 'end_time')
+    list_display = ('number', 'start_time', 'end_time')
+
+
+@admin.register(LessonType)
+class LessonTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(TimetableEntry)
+class TimetableEntryAdmin(admin.ModelAdmin):
+    list_display = ('week', 'day', 'group', 'lesson_number', 'subject', 'teacher', 'lesson_type')
+    list_filter = ('week', 'day', 'group__course__faculty', 'group__course__number')
+    search_fields = ('subject__name', 'teacher__last_name')
