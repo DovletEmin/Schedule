208ee2d935fbf03319325f3f5d6b2608207107a5
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-11-17 16:41:28 +0500

    Added CORS settings

--- src/src/settings.py
+++ src/src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_#b(ki8a_m(5x-vygwj$cf9#gwr-&*c6z7asr)@qjr2y%bnte&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    'corsheaders',
     'timetable',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.55.43:3000"
 ]
 
 ROOT_URLCONF = 'src.urls'
