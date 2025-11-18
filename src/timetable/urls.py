1526b3b49d3e1661ee9f0a4162c6242605ab6ede
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-11-18 12:03:50 +0500

    Added new Week API endpoint url path

--- src/timetable/urls.py
+++ src/timetable/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import TimetableSearchAPIView, WeekDayScheduleAPIView
+from .views import TimetableSearchAPIView, WeekDayScheduleAPIView, WeekScheduleAPIView
 
 urlpatterns = [
     path(
         '<int:week_number>/<int:day_number>/',
         WeekDayScheduleAPIView.as_view(),
         name='week-day-schedule'
+    ),
+    path(
+        '<int:week_number>/',
+        WeekScheduleAPIView.as_view(),
+        name='week-schedule'
     ),
     path("search/", TimetableSearchAPIView.as_view(), name="timetable-search"),
 ]
