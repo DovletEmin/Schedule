9428774b18574e3f7d31d784916ebaaf96b1c3c7
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-12-19 14:28:21 +0500

    Remove Docker deployment and revert settings/requirements

--- src/src/settings.py
+++ src/src/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,15 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-_#b(ki8a_m(5x-vygwj$cf9#gwr-&*c6z7asr)@qjr2y%bnte&",
-)
+SECRET_KEY = "django-insecure-_#b(ki8a_m(5x-vygwj$cf9#gwr-&*c6z7asr)@qjr2y%bnte&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
